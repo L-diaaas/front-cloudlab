251ab3a974c335ab4ed22e00aa613863d2187dfc
Laura Dias <lauradiasdesouza04@gmail.com>
2025-11-25 11:43:04 -0300

    Add files via upload

---
+++ js/calendar.js
@@ -0,0 +1,69 @@
+const daysContainer = document.getElementById("days");
+const monthYear = document.getElementById("monthYear");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+let date = new Date();
+
+const markedDates = [
+  { day: 29, month: 10, year: 2025 },  
+];
+
+/* No JavaScript, os meses começam do zero:
+    janeiro: 0
+    favereiro: 1
+    ...
+*/
+
+
+function renderCalendar() {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  const firstDay = new Date(year, month, 1).getDay();
+  const totalDays = new Date(year, month + 1, 0).getDate();
+
+  monthYear.textContent = date.toLocaleString("pt-BR", {
+    month: "long",
+    year: "numeric",
+  });
+
+  daysContainer.innerHTML = "";
+
+  for (let i = 0; i < firstDay; i++) {
+    daysContainer.innerHTML += `<div></div>`;
+  }
+
+  for (let day = 1; day <= totalDays; day++) {
+    const isToday =
+      day === new Date().getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear();
+
+    const isMarked = markedDates.some(d =>
+      d.day === day && d.month === month && d.year === year
+    );
+
+    daysContainer.innerHTML += `
+      <div class="
+        ${isToday ? 'today' : ''} 
+        ${isMarked ? 'marked' : ''}
+      ">
+        ${day}
+      </div>
+    `;
+  }
+}
+
+prevBtn.onclick = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+};
+
+nextBtn.onclick = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+};
+
+
+renderCalendar();
